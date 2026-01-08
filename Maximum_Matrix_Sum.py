ba6a11e608c63f09427decccd9a5e0a65291290e
TheGuja <jiag2023@gmail.com>
2026-01-08 16:45:30 -0500

    January 4

---
+++ Maximum_Matrix_Sum.py
@@ -0,0 +1,21 @@
+import math
+
+def maxMatrixSum(matrix) -> int:
+    numNegatives = 0
+    minValue = math.inf
+    totalSum = 0
+
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+            curr = matrix[row][col]
+            minValue = min(minValue, abs(curr))
+
+            if curr < 0:
+                numNegatives += 1
+
+            totalSum += abs(curr)
+
+    if numNegatives % 2 == 0:
+        return totalSum
+    else: # numNegatives % 2 == 1
+        return totalSum - (2 * minValue)
